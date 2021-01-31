6e0475a4654f4af60fe7da8873bc73bfd9ff7b85
rmartm14 <rmartm14@estudiantes.unileon.es>
2021-01-31 22:50:33 +0100

    Aestethic changes

--- ImageView.py
+++ ImageView.py
@@ -30,18 +30,54 @@ itemSelectorColumn = [
 
 ]
 
+# image visualizer
+imageViewerColumn = [
+    [sg.Text("Choose an image from list on left")],
+    [sg.Text(size=(40, 1), key="-TOUT-")],
+    [sg.Image(k="-IMAGE")]
+]
+
 layout = [
     [sg.Column(itemSelectorColumn),
      sg.VSeparator(),
+     sg.Column(imageViewerColumn)
      ]
 
 ]
 
 # window
-window = sg.Window(title=title, layout=layout)
+window = sg.Window(title=title, layout=layout,
+                   size=(1000, 1000))
 
 while True:
     event, values = window.read()
+    # Close the window
     if event == sg.WIN_CLOSED:
         break
+    # Event of the input
+    if event == "-FOLDER-":
+        folder = values["-FOLDER-"]
+        try:
+            file_list = os.listdir(folder)
+        except:
+            file_list = []
+
+        fnames = [
+            f
+            for f in file_list
+            if os.path.isfile(os.path.join(folder, f))
+            and f.lower().endswith((".png", ".gif"))
+        ]
+        window["-FILE LIST-"].update(fnames)
+    # File chosen
+    elif event == "-FILE LIST-":
+        try:
+            filename = os.path.join(
+                values["-FOLDER-"], values["-FILE LIST-"][0]
+            )
+            window["-TOUT-"].update(filename)
+            window["-IMAGE"].update(filename=filename)
+        except:
+            pass
+
 window.close()
